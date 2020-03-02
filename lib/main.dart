ffafe73a9aafc16856bfedd3bc1f630ae204b202
Lucas Tonussi <lucastonussi@gmail.com>
2020-03-02 14:44:22 -0300

    add

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:busca_gif/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:busca_gif/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
